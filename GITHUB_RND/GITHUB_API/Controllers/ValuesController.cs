16d87db306c970e0062be3de906e25519d983e17
satishazure2021 <er.satishazure2021@gmail.com>
2021-07-12 08:41:40 +0100

    Br 1 changes

--- GITHUB_RND/GITHUB_API/Controllers/ValuesController.cs
+++ GITHUB_RND/GITHUB_API/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace GITHUB_API.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","Vedant","Satish" };
         }
 
         // GET api/values/5
