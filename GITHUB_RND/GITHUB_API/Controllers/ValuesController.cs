42d75994baabe1650b8fbbc26e0654065a9ce067
satishazure2021 <er.satishazure2021@gmail.com>
2021-07-14 10:06:43 +0100

    Added Monita Prasad in Get Request

--- GITHUB_RND/GITHUB_API/Controllers/ValuesController.cs
+++ GITHUB_RND/GITHUB_API/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace GITHUB_API.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2","Vedant","Satish","Vijay", "Tanisha","Prabakran","Monita" };
+            return new string[] { "value1", "value2","Vedant","Satish","Vijay", "Tanisha","Prabakran","Monita Prasad" };
         }
 
         // GET api/values/5
